fad6e147f3a80a6d3dd69ec65bd176d2e874de9a
zekaryas <zekaryasdinku@gmail.com>
2019-08-05 23:33:45 +1000

    configuration for filter dialog

--- app/src/main/java/com/ussz/jobify/fragments/exploreFragments/ExploreFragment.java
+++ app/src/main/java/com/ussz/jobify/fragments/exploreFragments/ExploreFragment.java
@@ -1,6 +1,7 @@
 package com.ussz.jobify.fragments.exploreFragments;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,9 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import com.google.android.material.tabs.TabLayout;
 import com.ussz.jobify.adapters.ViewPagerAdapter;
@@ -27,6 +31,8 @@ import java.util.List;
  */
 public class ExploreFragment extends Fragment {
 
+
+    private Dialog dialog;
 
     public ExploreFragment() {
         // Required empty public constructor
@@ -61,10 +67,41 @@ public class ExploreFragment extends Fragment {
         viewPager.setAdapter(evpa);
         tabLayout.setupWithViewPager(viewPager);
 
+        rootView.findViewById(R.id.filterFAB).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeAndShowDialog();
+            }
+        });
+
         return rootView;
     }
 
 
+
+    private void makeAndShowDialog(){
+        dialog = new Dialog(getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.filterdialog);
+        dialog.setCancelable(true);
+
+
+        ((ImageButton) dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+    }
 
 
 
