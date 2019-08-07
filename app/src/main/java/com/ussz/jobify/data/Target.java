9797e7b255c8fc800b7075b3fa0014a03422a7f4
SimretA <simretaraya7@gmail.com>
2019-08-07 10:27:47 +0300

    ellipsized

--- app/src/main/java/com/ussz/jobify/data/Target.java
+++ app/src/main/java/com/ussz/jobify/data/Target.java
@@ -1,5 +1,12 @@
 package com.ussz.jobify.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Target {
 
     private String department;
