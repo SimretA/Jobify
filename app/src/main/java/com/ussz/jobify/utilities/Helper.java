7a3ece52d7ce176b2f13e37bed69d2dc09b43347
zekaryas <zekaryasdinku@gmail.com>
2019-08-12 21:32:31 +1000

    from random color to color by letter

--- app/src/main/java/com/ussz/jobify/utilities/Helper.java
+++ app/src/main/java/com/ussz/jobify/utilities/Helper.java
@@ -4,12 +4,11 @@ import java.util.Random;
 
 public class Helper {
 
-    private static Random random = new Random();
     private static String[] colors = new String[]{"#C10000FF","#800000","#228B22","#9932CC","#696969"};
 
 
-    public static String getRandomColorString(){
-        return colors[random.nextInt(colors.length)];
+    public static String getRandomColorString(char letter){
+        return colors[((int)letter)%colors.length];
     }
 
 }
